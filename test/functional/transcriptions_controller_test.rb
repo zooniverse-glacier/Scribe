66cff16f8a9b30ed10f03c70b81f40877f67a32b
Stuart Lynn <stuart.lynn@gmail.com>
2011-07-14 16:24:40 +0100

    fixing redirect text

--- test/functional/transcriptions_controller_test.rb
+++ test/functional/transcriptions_controller_test.rb
@@ -11,7 +11,8 @@ class TranscriptionsControllerTest < ActionController::TestCase
     context "#new not logged in" do
       setup do
         @template = Factory :template
-        @asset = Factory :asset
+        @asset_collection = Factory :asset_collection
+        @asset = Factory :asset, :asset_collection=>@asset_collection
 
         get :new
       end
@@ -19,7 +20,7 @@ class TranscriptionsControllerTest < ActionController::TestCase
       should respond_with :redirect
       
       should "redirect_to '/cas server'" do
-        assert_redirected_to 'https://login.zooniverse.org/login?service=http%3A%2F%2Ftest.host%2Ftranscriptions%2Fnew'
+        assert_redirected_to "https://login.zooniverse.org/login?service=http%3A%2F%2Ftest.host%2Ftranscriptions%2Fnew"
       end
     end
     
